6ed7ca485cc1b7518808ea9bbbbff0c19f1fc083
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-12-09 14:00:36 +0300

    solid

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,33 +17,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-/*
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
-}
-*/
